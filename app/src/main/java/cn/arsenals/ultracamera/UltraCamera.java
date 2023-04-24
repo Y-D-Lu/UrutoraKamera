f73980adb4190cf2a65cd2f1947193839a36c38a
luyuedong <e4256318151@gmail.com>
2023-04-24 13:05:20 +0800

    add log util for print stack trace

--- app/src/main/java/cn/arsenals/ultracamera/UltraCamera.java
+++ app/src/main/java/cn/arsenals/ultracamera/UltraCamera.java
@@ -1,5 +1,14 @@
 package cn.arsenals.ultracamera;
 
+import android.util.Log;
+
 public class UltraCamera {
-    private static final int aaa = R.attr.actionBarDivider;
+    private static final String TAG = "luyuedong666 - UltraCamera";
+
+    public static void printLogAndStackTrace() {
+        Log.e(TAG, "printLogAndStackTrace start");
+        Throwable tr = new Throwable();
+        tr.printStackTrace();
+        Log.e(TAG, "printLogAndStackTrace end", tr);
+    }
 }
